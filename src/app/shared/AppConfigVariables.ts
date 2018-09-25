b8b6f9e4b1bbb2a739d449dd035ac95e33a499e4
Carlo Bramini <braminicarlo@live.com>
2018-09-25 13:01:02 +0200

    added AppConfigVariables.ts to gitignore

--- src/app/shared/AppConfigVariables.ts
+++ src/app/shared/AppConfigVariables.ts
@@ -8,7 +8,6 @@ export class AppConfigVariables {
     // }
 
     public static baseUrlBackend():string{
-        // throw new Error('zr not implemented: AppConfigVariables.baseUrlBackend()');
         return "http://localhost:3010/";
     }
 
